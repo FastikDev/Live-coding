93c8d5347ea92af34aa82728f7a11e2c9e408509
Viktor Troyan <149237046+FastikDev@users.noreply.github.com>
2024-07-10 09:51:49 +0300

    Update student.js

--- Live-8/task-1/student.js
+++ Live-8/task-1/student.js
@@ -3,17 +3,17 @@ import { User } from './user.js';
 export class Student extends User {
   #isActive = true;
 
-  constructor (id, firstName, lastName, group, points) {
-    super (id, firstName, lastName);
+  constructor(id, firstName, lastName, group, points) {
+    super(id, firstName, lastName);
     this.group = group;
     this.points = points;
   }
 
-  get getIsActive () {
+  get getIsActive() {
     return this.#isActive;
   }
 
-  set setIsActive (value) {
+  set setIsActive(value) {
     if (typeof value !== 'boolean') {
       console.error('value is not a boolean type');
       return;
